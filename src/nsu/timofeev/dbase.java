ce0ebff401c16334490b614ac2f39d2a0981fe27
Ivan <tsyrus@live.com>
2020-09-19 11:08:32 +0700

    added delAuto()

--- src/nsu/timofeev/dbase.java
+++ src/nsu/timofeev/dbase.java
@@ -7,31 +7,42 @@ class dbase {
     private ArrayList<Auto> base = new ArrayList<Auto>();
 
     public void printBase(Scanner in) {
-        for (int i = 0; i < base.size(); i++) {
-            System.out.printf("%d. %s %s (%d)\n", i+1, base.get(i).getManufacturer(), base.get(i).getModel(), base.get(i).getYear());
-        }
-        while (true) {
-            System.out.print("Choose auto (0 to main menu): ");
-            int ans = in.nextInt();
-            if (ans == 0) {
-                break;
+        if (base.size() == 0) {
+            System.out.println("\nThere are no records in database!");
+        } else {
+            while (true) {
+                for (int i = 0; i < base.size(); i++) {
+                    System.out.printf("%d. %s %s (%d)\n", i + 1, base.get(i).getManufacturer(), base.get(i).getModel(), base.get(i).getYear());
+                }
+                System.out.print("Choose auto (0 to main menu): ");
+                int ans = in.nextInt();
+                if (ans == 0) {
+                    break;
+                }
+                infoMore(ans - 1, in);
             }
-            infoMore(ans - 1, in);
         }
     }
 
     private void infoMore(int ans, Scanner in) {
-        System.out.printf("\nAbout auto #%d\n\n", ans);
-        System.out.printf("Manufacturer: %s\n", base.get(ans).getManufacturer());
-        System.out.printf("Model: %s\n", base.get(ans).getModel());
-        System.out.printf("Year: %d\n", base.get(ans).getYear());
-        System.out.printf("Body Type: %s", base.get(ans).getBodyType());
-        System.out.printf("\n1.Edit info\n2.Delete auto\n0.Back to list\n");
-        ans = in.nextInt();
+        while (true) {
+            System.out.printf("\nAbout auto #%d\n\n", ans);
+            System.out.printf("Manufacturer: %s\n", base.get(ans).getManufacturer());
+            System.out.printf("Model: %s\n", base.get(ans).getModel());
+            System.out.printf("Year: %d\n", base.get(ans).getYear());
+            System.out.printf("Body Type: %s", base.get(ans).getBodyType());
+            System.out.printf("\n1.Edit info\n2.Delete auto\n0.Back to list\n~base: ");
+            int ans2 = in.nextInt();
+            switch (ans2) {
+                case 2: delAuto(ans); return;
+                case 0: return;
+            }
+        }
     }
 
-    private void delAuto() {
-
+    private void delAuto(int index) {
+        base.remove(index);
+        System.out.printf("\nDELETED!\n");
     }
 
     public void addAuto(Scanner in) {
